8c8d77798730941625c9e39eff6f93d0705a0354
mesutAygun1881 <mesut>
2021-07-20 12:56:32 +0300

    Initial Commit

---
+++ MovieManagerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieManagerApp
+//
+//  Created by Mesut Aygün on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
